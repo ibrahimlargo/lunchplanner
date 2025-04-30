951c77df3a817f08f839accbe1785294b9069b89
Sarah Löhnert <sarah.loehnert@check24.de>
2025-04-30 13:38:16 +0200

    created CatererFactory and FeedbackResultFactory and added is_admin to UserFactory

---
+++ api/database/factories/CatererFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Caterer>
+ */
+class CatererFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->company(),
+            'email' => $this->faker->email(),
+            'phone' => $this->faker->phoneNumber(),
+            'order_url' => $this->faker->url(),
+            'address' => $this->faker->address(),
+        ];
+    }
+}
---
+++ api/database/factories/FeedbackResultFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\DishChoice;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\FeedbackResult>
+ */
+class FeedbackResultFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'comment' => $this->faker->sentence(),
+            'rating' => $this->faker->numberBetween(1, 5),
+            'dish_choice' => DishChoice::query()->inRandomOrder()->first() ?? DishChoice::factory()->create(),
+        ];
+    }
+}
