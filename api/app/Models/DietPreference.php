32d4cc4b0ef4d363e86f185575116e9db7f7afe4
Sarah Löhnert <sarah.loehnert@check24.de>
2025-04-30 14:06:52 +0200

    added DietPreferencesEnum

--- api/app/Models/DietPreference.php
+++ api/app/Models/DietPreference.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use App\Enums\DietPreferencesEnum;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class DietPreference extends Model
 {
+    protected $casts = [
+        'name' => DietPreferencesEnum::class
+    ];
     public function dishes(): HasMany
     {
         return $this->hasMany(Dish::class);
