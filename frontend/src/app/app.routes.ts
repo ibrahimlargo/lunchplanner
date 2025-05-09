fa3e024de44aca8512dbc7bbff6d5770387932fc
Carlo Troise <carlo.troise@check24.de>
2025-05-09 08:13:10 +0200

    small changes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import {Routes} from '@angular/router';
 import {AppComponent} from './app.component';
-import {KontaktComponent} from './kontakt/kontakt.component';
 
 export const routes: Routes = [
   {path: '', component: AppComponent},
-  {path: 'hello', component: KontaktComponent}
 ];
